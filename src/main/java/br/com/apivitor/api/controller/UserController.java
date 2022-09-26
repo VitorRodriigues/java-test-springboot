7cd2a25b4f62f76fd23ed871b603a1096ace5843
Vitor Rodrigues <tdvse@hotmail.com>
2022-09-26 11:16:01 -0300

    retornando dto para user controller

--- src/main/java/br/com/apivitor/api/controller/UserController.java
+++ src/main/java/br/com/apivitor/api/controller/UserController.java
@@ -1,7 +1,9 @@
 package br.com.apivitor.api.controller;
 
 import br.com.apivitor.api.model.UserModel;
+import br.com.apivitor.api.model.dto.UserDto;
 import br.com.apivitor.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService userService;
 
     @GetMapping(value = "/findById/{id}")
-    public ResponseEntity<UserModel> findById(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
+    public ResponseEntity<UserDto> findById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(mapper.map(userService.findById(id), UserDto.class));
     }
 }
--- src/main/java/br/com/apivitor/api/model/dto/UserDto.java
+++ src/main/java/br/com/apivitor/api/model/dto/UserDto.java
@@ -1,5 +1,6 @@
 package br.com.apivitor.api.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ public class UserDto {
     private Long id;
     private String name;
     private String email;
+
+    @JsonIgnore
     private String password;
-
-
 }
